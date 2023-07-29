451ff0c769386edfda26f05460722cba54bff93b
Rok-Pahic <r.pahic@gmail.com>
2023-07-29 19:17:50 +0200

    icons

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,14 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-
+html_static_path = ['_static']
+html_logo = "figures_main_page/reconcycle-transparent.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+## Icon
+html_favicon = "favicon.ico"
